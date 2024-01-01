612c0570c4484cbca3a826681a9ce464a1ee9b5d
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-01-01 14:15:02 +0900

    다섯번째 공부

--- TPS/Assets/02.Scripts/PlayerCtrl.cs
+++ TPS/Assets/02.Scripts/PlayerCtrl.cs
@@ -6,6 +6,9 @@ public class PlayerCtrl : MonoBehaviour
 {
     //컴포넌트 캐시 처리할 변수
     private Transform tr;
+    // Animation 컴포넌트를 저장할 변수
+    private Animation anim;
+
     //이동 속도 변수(public으로 선언되어 인스펙터 뷰에 노출됨
     public float moveSpeed = 10.0f;
     //회전 속도 변수
@@ -16,7 +19,10 @@ public class PlayerCtrl : MonoBehaviour
     {
         //transform 컴포넌트를 추출해 변수에 대입
         tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
 
+        //애니메이션 실행
+        anim.Play("Idle");
         
     }
 
@@ -30,7 +36,7 @@ public class PlayerCtrl : MonoBehaviour
         //Debug.Log("h =" + h);
         //Debug.Log("v =" + v);
 
-        //전후 좌우 이동 백터 게싼
+        //전후 좌우 이동 백터 계산
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
         //translate 함수를 사용한 이동 로직
@@ -39,6 +45,33 @@ public class PlayerCtrl : MonoBehaviour
         //Vector3.up 축을 기준으로 turnSpeed만큼 속도로 회전
         tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
 
-        
+        //주인공 캐릭터의 애니메이션 설정
+        PlayerAnim(h, v);
+    }
+
+    void PlayerAnim(float h, float v)
+    {
+        //키보드 입력값을 기준으로 동작할 애니메이션 수행
+
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.25f);      //전진 애니메이션 실행
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.25f);      //후진 애니메이션 실행
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade("RunR", 0.25f);      //오른쪽 이동 애니메이션 실행
+        }
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade("RunL", 0.25f);      //왼쪽 이동 애니메이션 실행
+        }
+        else
+        {
+            anim.CrossFade("Idle", 0.25f);      //정지시 Idle 애니메이션 실행
+        }
     }
 }
