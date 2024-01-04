3e18d436e9c948016bac0e598f553d560ae3a4b0
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-01-04 14:27:13 +0900

    여덟번째 자습

---
+++ TPS/Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    //총알의 파괴력
+    public float damage = 20.0f;
+
+    //총알 발사 힘
+    public float force = 1500.0f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Rigidbody 컴포넌트를 추출
+        rb = GetComponent<Rigidbody>();
+
+        //총알의 전진 방향으로 힘을 가진다
+        rb.AddForce(transform.forward * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
