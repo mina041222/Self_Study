57243285706b5f3be8faa1777adb232def4c9d19
mina041312 <minajeon2004@gmail.com>
2024-01-07 08:48:05 +0900

    아홉번째 자습

---
+++ TPS/Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    //충돌이 시작할 떄 발생한는 이벤트
+    void OnCollisionEnter(Collision coll)
+    {
+        //충돌한 게임 오브젝트 태그값 비교
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            //충돌한 게임 오브젝트 삭제
+            Destroy(coll.gameObject);
+        }
+    }
+}
