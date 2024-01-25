89d87972850eaf9b187b22f5fb3227f70555d335
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-01-25 15:10:44 +0900

    열한 번째 자습

--- TPS/Assets/02.Scripts/BarrelCtrl.cs
+++ TPS/Assets/02.Scripts/BarrelCtrl.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class BarrelCtrl : MonoBehaviour
 {
+    //폭팔 효과 파티클을 연결할 변수
+    public GameObject expEffext;
     //무작위로 적용할 텍스쳐 배열
     public Texture[] textures;
+    //폭팔 반경
+    public float radius = 10.0f;
     //하위에 있는 Mesh Renderer 컴포넌트를 저장할 변수
     private new MeshRenderer renderer;
 
@@ -47,12 +51,39 @@ public class BarrelCtrl : MonoBehaviour
 
     void ExpBarrel()
     {
+        //폭팔 효과 파티클 생성
+        GameObject exp = Instantiate(expEffext, tr.position, Quaternion.identity);
+        //폭팔 효과 파티클 5초후에 제거
+        Destroy(exp, 5.0f);
         //Rigidbody 컴포넌트의 mass를 1.0으로 수정해 무게를 가볍게 함
-        rb.mass = 1.0f;
+        //rb.mass = 1.0f;
         //위로 솟구치게 힘을 가함
-        rb.AddForce(Vector3.up * 1500.0f);
+        //rb.AddForce(Vector3.up * 1500.0f);
+
+        // 간접 폭발력 전달
+        IndirectDamage(tr.position);
 
         //3초 후에 드럼통 제거
         Destroy(gameObject, 3.0f);
+
+    }
+
+    //폭팔력을 주변에 전달하는 함수
+    void IndirectDamage(Vector3 pos)
+    {
+        //주변에 있는 드럼통을 모두 추출
+        Collider[] colls = Physics.OverlapSphere(pos, radius, 1 << 3);
+
+        foreach(var coll in colls)
+        {
+            //폭팔 범위에 포함된 드럼통의 Rigidbody 컴포넌트 추출
+            rb = coll.GetComponent<Rigidbody>();
+            //드럼통 무게를 가볍게 함
+            rb.mass = 1.0f;
+            //FreezeRotation 제한 값을 해제
+            rb.constraints = RigidbodyConstraints.None;
+            //폭팔력을 전달
+            rb.AddExplosionForce(1500.0f, pos, radius, 1200.0f);
+        }
     }
 }
--- TPS/Assets/02.Scripts/RemoveBullet.cs
+++ TPS/Assets/02.Scripts/RemoveBullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    //스파크 파티클 프리팹을 연결할 변수
+    public GameObject sparkEffect;
 
     //충돌이 시작할 떄 발생한는 이벤트
     void OnCollisionEnter(Collision coll)
@@ -11,7 +13,15 @@ public class RemoveBullet : MonoBehaviour
         //충돌한 게임 오브젝트 태그값 비교
         if (coll.collider.CompareTag("BULLET"))
         {
+            //첫번째 충돌 지점의 정보 추출
+            ContactPoint cp = coll.GetContact(0);
+            //충돌한 총알의 법선 백터를 쿼터니언 타입으로 변환
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
 
+            //스파크 파티클을 동적으로 생성
+            GameObject spark = Instantiate (sparkEffect, cp.point, rot);
+            //일정시간이 지난후 스파클 파티클을 삭제
+            Destroy(spark, 0.5f);
             //충돌한 게임 오브젝트 삭제
             Destroy(coll.gameObject);
         }
