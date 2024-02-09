672ef408d34c27b78a0a11235cf6167555eec889
mina041222 <minajeon2004@g.shingu.ac.kr>
2024-02-09 15:11:16 +0900

    열여섯번째 자습

---
+++ TPS/Assets/02.Scripts/DelegateDemo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DelegateDemo : MonoBehaviour
+{
+    //델리게이트 선언
+    delegate float SumHandler(float a, float b)
+    //델리게이트 타입의 변수 선언
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TPS/Assets/02.Scripts/MonsterCtrl.cs
+++ TPS/Assets/02.Scripts/MonsterCtrl.cs
@@ -33,6 +33,8 @@ public class MonsterCtrl : MonoBehaviour
     private readonly int hashTrace = Animator.StringToHash("IsTrace");
     private readonly int hashAttack = Animator.StringToHash("IsAttack");
     private readonly int hashHit = Animator.StringToHash("Hit");
+    private readonly int hashPlayerDie = Animator.StringToHash("PlayerDie");
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
     //혈흔 효과 프리팹
     private GameObject bloodEffect;
 
@@ -167,6 +169,22 @@ public class MonsterCtrl : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, attackDist);
         }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log(coll.gameObject.name);
+    }
+
+    void OnPlayerDie()
+    {
+        //몬스터의 상태를 체크하는 코루틴 함수를 모두 정지시킴
+        StopAllCoroutines();
+
+        //추적을 정지하고 애니메이션을 수행
+        agent.isStopped = true;
+        anim.SetFloat(hashSpeed, Random.Range(0.8f, 1.2f));
+        anim.SetTrigger(hashPlayerDie);
     }
 }
 
